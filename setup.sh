dfaa7d4ab82dded78336cee0022dbb28aa333c3c
tbs <tbs@localhost.localdomain>
2022-09-02 16:06:09 -0300

    remove dotfiles manager script

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-git clone --bare https://github.com/andrebossi/dotfiles.git $HOME/.dotfiles
-echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.bash_aliases
-source $HOME/.bash_aliases
-dotfiles checkout
