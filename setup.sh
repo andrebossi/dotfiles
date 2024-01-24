d0b23fe2a8b015680c006272a5dbe429578efa42
Andre Bossi <andreluizbossi70@gmail.com>
2024-01-24 10:52:39 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -37,7 +37,7 @@ runAsRoot() {
 
 # First parameter update apps
 UPDATE=$1
-if [ -z $UPDATE ]; then
+if [ -z "$UPDATE" ]; then
   UPDATE=0
 else
   echo "Upgrade latest version"
@@ -51,32 +51,32 @@ fi
 mkdir $WORKDIR && cd $WORKDIR
 
 # Install jq
-if ([ ! -f "$INSTALL_DIR/jq" ] || [ $UPDATE -eq 1 ]); then
+if ([ ! -f "$INSTALL_DIR/jq" ] || [ "$UPDATE" -eq 1 ]); then
   wget https://github.com/stedolan/jq/releases/latest/download/jq-linux64 -O jq
   runAsRoot install -o root -g root -m 0755 jq "$INSTALL_DIR/jq"
 fi
 
 # Install yq
-if ([ ! -f "$INSTALL_DIR/yq" ] || [ $UPDATE -eq 1 ]); then
+if ([ ! -f "$INSTALL_DIR/yq" ] || [ "$UPDATE" -eq 1 ]); then
   wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O yq
   runAsRoot install -o root -g root -m 0755 yq "$INSTALL_DIR/yq"
 fi
 
 # Install Kubectl
-if ([ ! -f "$INSTALL_DIR/kubectl" ] || [ $UPDATE -eq 1 ]); then
+if ([ ! -f "$INSTALL_DIR/kubectl" ] || [ "$UPDATE" -eq 1 ]); then
   echo "Install kubectl"
   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
   runAsRoot install -o root -g root -m 0755 kubectl "$INSTALL_DIR/kubectl"
 fi
 
 # Install Helm
-if ([ ! -f "$INSTALL_DIR/helm" ] || [ $UPDATE -eq 1 ]); then
+if ([ ! -f "$INSTALL_DIR/helm" ] || [ "$UPDATE" -eq 1 ]); then
   echo "Install helm"
   curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 fi
 
 # Install Kind
-if ([ ! -f "$INSTALL_DIR/kind" ] || [ $UPDATE -eq 1 ]); then
+if ([ ! -f "$INSTALL_DIR/kind" ] || [ "$UPDATE" -eq 1 ]); then
   echo "Install Kind"
   KIND_LATEST=$(get_latest_release "kubernetes-sigs/kind")
   curl -sSL -o kind "https://github.com/kubernetes-sigs/kind/releases/download/$KIND_LATEST/kind-${OS}-${ARCH}"
@@ -84,7 +84,7 @@ if ([ ! -f "$INSTALL_DIR/kind" ] || [ $UPDATE -eq 1 ]); then
 fi
 
 # Install Velero
-if ([ ! -f "$INSTALL_DIR/velero" ] || [ $UPDATE -eq 1 ]); then
+if ([ ! -f "$INSTALL_DIR/velero" ] || [ "$UPDATE" -eq 1 ]); then
   echo "Install velero"
   VELERO_LATEST=$(get_latest_release "vmware-tanzu/velero")
   curl -sSL -o velero.tar.gz "https://github.com/vmware-tanzu/velero/releases/latest/download/velero-$VELERO_LATEST-${OS}-${ARCH}.tar.gz"
@@ -93,7 +93,7 @@ if ([ ! -f "$INSTALL_DIR/velero" ] || [ $UPDATE -eq 1 ]); then
 fi
 
 # Install Terraform
-if ([ ! -f "$INSTALL_DIR/terraform" ] || [ $UPDATE -eq 1 ]); then
+if ([ ! -f "$INSTALL_DIR/terraform" ] || [ "$UPDATE" -eq 1 ]); then
   echo "Install Terraform"
   TF_VERSION=$(get_latest_release "hashicorp/terraform")
   TF_VERSION=${TF_VERSION:1}
@@ -104,7 +104,7 @@ if ([ ! -f "$INSTALL_DIR/terraform" ] || [ $UPDATE -eq 1 ]); then
 fi
 
 # Install ArgoCLI
-if ([ ! -f "$INSTALL_DIR/argocd" ] || [ $UPDATE -eq 1 ]); then
+if ([ ! -f "$INSTALL_DIR/argocd" ] || [ "$UPDATE" -eq 1 ]); then
   echo "Install ArgoCLI"
   curl -sSL -o argocd-${OS}-${ARCH} "https://github.com/argoproj/argo-cd/releases/latest/download/argocd-${OS}-${ARCH}"
   runAsRoot install -m 555 argocd-${OS}-${ARCH} "$INSTALL_DIR/argocd"
@@ -112,14 +112,14 @@ if ([ ! -f "$INSTALL_DIR/argocd" ] || [ $UPDATE -eq 1 ]); then
 fi
 
 # Install ArgoCLI Rollout
-if ([ ! -f "$INSTALL_DIR/kubectl-argo-rollouts" ] || [ $UPDATE -eq 1 ]); then
+if ([ ! -f "$INSTALL_DIR/kubectl-argo-rollouts" ] || [ "$UPDATE" -eq 1 ]); then
   echo "Install Argo Rollouts"
   curl -LO "https://github.com/argoproj/argo-rollouts/releases/latest/download/kubectl-argo-rollouts-${OS}-${ARCH}"
   runAsRoot install -m 755 kubectl-argo-rollouts-${OS}-${ARCH} "$INSTALL_DIR/kubectl-argo-rollouts"
 fi
 
 # Install kubectx
-if ([ ! -f "$INSTALL_DIR/kubectx" ] || [ $UPDATE -eq 1 ]); then
+if ([ ! -f "$INSTALL_DIR/kubectx" ] || [ "$UPDATE" -eq 1 ]); then
   echo "Install kubectx"
   KUBECTX_LATEST=$(get_latest_release "ahmetb/kubectx")
   curl -sSL -o kubectx.tar.gz https://github.com/ahmetb/kubectx/releases/download/$KUBECTX_LATEST/kubectx_${KUBECTX_LATEST}_${OS}_x86_64.tar.gz
@@ -128,7 +128,7 @@ if ([ ! -f "$INSTALL_DIR/kubectx" ] || [ $UPDATE -eq 1 ]); then
 fi
 
 # Install kubens
-if ([ ! -f "$INSTALL_DIR/kubens" ] || [ $UPDATE -eq 1 ]); then
+if ([ ! -f "$INSTALL_DIR/kubens" ] || [ "$UPDATE" -eq 1 ]); then
   echo "Install kubens"
   KUBENS_LATEST=$(get_latest_release "ahmetb/kubectx")
   curl -sSL -o kubens.tar.gz https://github.com/ahmetb/kubectx/releases/download/$KUBENS_LATEST/kubens_${KUBENS_LATEST}_${OS}_x86_64.tar.gz
@@ -137,7 +137,7 @@ if ([ ! -f "$INSTALL_DIR/kubens" ] || [ $UPDATE -eq 1 ]); then
 fi
 
 # Install Pyenv
-if [ $UPDATE -eq "pyenv" ]; then
+if [ "$UPDATE" -eq "pyenv" ]; then
   if grep -q "ID_LIKE=debian" /etc/os-release ; then
     sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
       libbz2-dev libreadline-dev libsqlite3-dev curl \
